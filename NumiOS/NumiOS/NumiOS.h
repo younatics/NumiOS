00aae68cd5d42c064fab07b446a11b6b22ef8077
Seungyoun Yi <younatics@gmail.com>
2018-07-31 13:55:34 +0900

    add basic setting

---
+++ NumiOS/NumiOS/NumiOS.h
@@ -0,0 +1,19 @@
+//
+//  NumiOS.h
+//  NumiOS
+//
+//  Created by Seungyoun Yi on 2018. 7. 31..
+//  Copyright © 2018년 Seungyoun Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NumiOS.
+FOUNDATION_EXPORT double NumiOSVersionNumber;
+
+//! Project version string for NumiOS.
+FOUNDATION_EXPORT const unsigned char NumiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NumiOS/PublicHeader.h>
+
+
---
+++ NumiOS/NumiOSDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NumiOSDemo
+//
+//  Created by Seungyoun Yi on 2018. 7. 31..
+//  Copyright © 2018년 Seungyoun Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
